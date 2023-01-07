117a205573f146a9aab198491a42b951058b2931
shreyhere <116891326+shreyhere@users.noreply.github.com>
2023-01-07 20:44:35 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,22 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
+const relativeList=['relativeListItem1', 'relativeListItem2', 'relativeListItem3', 'relativeListItem4', 'relativeListItem5', 'relativeListItem6']
+
 class App extends Component {
     render() {
 
         return(
             <div id="main">
                {/* Do not remove the main div */}
+              <ol key="relativeList">
+                {
+                  relativeList.map(x => (
+                    <li key={x}>{x}</li>
+                  ))
+                }
+              </ol>
+                
             </div>
         )
     }
